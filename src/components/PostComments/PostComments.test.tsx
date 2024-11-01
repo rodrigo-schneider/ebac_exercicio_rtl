90bc1f4782b049585cd627b31520f120aa47e82f
Rodrigo Schneider <rodrigoschneiderscofano@gmail.com>
2024-11-01 06:24:34 -0300

    Testes feitos

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,48 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+const mock = [
+  {
+    id: 1,
+    comment: "Mensagem teste 1",
+  },
+  {
+    id: 2,
+    comment: "Mensagem teste 2",
+  },
+];
+
+let index = 0;
+let mensagem = mock[index].comment;
+
+function mudamensagem() {
+  index = index + 1;
+  mensagem = mock[index].comment;
+}
+
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<Post />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  test("deve renderizar o primeiro comentario", async () => {
+    render(<Post />);
+    const botao = screen.getByTestId("btn-comentario");
+    const Textarea = screen.getByTestId("textArea");
+    fireEvent.change(Textarea, { target: { value: mensagem } });
+    fireEvent.click(botao);
+    expect(screen.getByText("Olá marilene")).toBeInTheDocument();
+
+    mudamensagem();
+  });
+
+  test("deve renderizar segundo comentario", async () => {
+    render(<Post />);
+    const botao = screen.getByTestId("btn-comentario");
+    const Textarea = screen.getByTestId("textArea");
+    fireEvent.change(Textarea, { target: { value: mensagem } });
+    fireEvent.click(botao);
+    expect(screen.getByText("Quero um desse")).toBeInTheDocument();
+  });
 });
